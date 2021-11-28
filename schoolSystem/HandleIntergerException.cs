9505cb35bbb6209fbc20d4742087b46e41e24687
Shehab osama <shehabosama0122421@gmail.com>
2021-11-28 05:10:10 +0200

    add initial Exception Concept

---
+++ schoolSystem/HandleIntergerException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace schoolSystem
+{
+	class HandleIntergerException : Exception
+	{
+		public HandleIntergerException(String message):base(message) { }
+	}
+}
